3111e0e31f575cf9fedba161e02edd817f854d14
Karthik Raja Nichenametla <96817647+karthikraja46@users.noreply.github.com>
2024-12-10 13:27:25 +0530

    Create EigenCAM_v8.py

---
+++ EigenCAM_v8.py
@@ -0,0 +1,72 @@
+import warnings
+warnings.filterwarnings('ignore')
+warnings.simplefilter('ignore')
+import torch
+import cv2
+import numpy as np
+import requests
+import torchvision.transforms as transforms
+from pytorch_grad_cam import EigenCAM
+from pytorch_grad_cam import GradCAM
+from pytorch_grad_cam.utils.image import show_cam_on_image, scale_cam_image
+from PIL import Image
+
+COLORS = np.random.uniform(0, 255, size=(80, 3))
+
+def parse_detections(results):
+    detections = results.pandas().xyxy[0]
+    detections = detections.to_dict()
+    boxes, colors, names = [], [], []
+
+    for i in range(len(detections["xmin"])):
+        confidence = detections["confidence"][i]
+        if confidence < 0.4:
+            continue
+        xmin = int(detections["xmin"][i])
+        ymin = int(detections["ymin"][i])
+        xmax = int(detections["xmax"][i])
+        ymax = int(detections["ymax"][i])
+        name = detections["name"][i]
+        category = int(detections["class"][i])
+        color = COLORS[category]
+
+        boxes.append((xmin, ymin, xmax, ymax))
+        colors.append(color)
+        names.append(name)
+    return boxes, colors, names
+
+
+def draw_detections(boxes, colors, names, img):
+    for box, color, name in zip(boxes, colors, names):
+        xmin, ymin, xmax, ymax = box
+        cv2.rectangle(
+            img,
+            (xmin, ymin),
+            (xmax, ymax),
+            color,
+            2)
+
+        cv2.putText(img, name, (xmin, ymin - 5),
+                    cv2.FONT_HERSHEY_SIMPLEX, 0.8, color, 2,
+                    lineType=cv2.LINE_AA)
+    return img
+image_path = '/Users/karthikrajanichenametla/X_AI_cassava/cotton.jpeg'
+img = np.array(Image.open(image_path))
+img = cv2.resize(img, (640, 640))
+rgb_img = img.copy()
+img = np.float32(img) / 255
+transform = transforms.ToTensor()
+tensor = transform(img).unsqueeze(0)
+
+import os
+model = torch.hub.load('ultralytics/yolov5', 'custom', '/Users/karthikrajanichenametla/X_AI_cassava/yolo_cam_env/best_yolov5_cotton.pt')  # custom/local model
+
+# model = torch.load("best.pt")
+model.eval()
+model.cpu()
+target_layers = [model.model.model.model[-3]]
+
+cam = EigenCAM(model, target_layers)
+grayscale_cam = cam(tensor)[0, :, :]
+cam_image = show_cam_on_image(img, grayscale_cam, use_rgb=True)
+Image.fromarray(cam_image).save("cam_output.jpg")
